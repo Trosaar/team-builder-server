fd514971bcdb707d8b15521d597546a6a53f1467
Aaron <candyman633@gmail.com>
2020-09-10 20:08:21 -0500

    added more complexity to schema

--- database/migrations/01_store.js
+++ database/migrations/01_store.js
@@ -12,6 +12,10 @@ exports.up = function(knex) {
         tbl.integer('price').unsigned().notNullable()
         tbl.string('name', 128).unique().notNullable()
         tbl.string('description', 240).notNullable()
+        tbl.string('size').references('size').inTable('item_sizes')
+        tbl.string('color').references('color').inTable('item_colors')
+        // tbl.enu('size', ['Extra Small', 'Small', 'Medium', 'Large', 'Extra Large'])
+        // tbl.string('color', 128)
     })
     .createTable('purchases', tbl => {
         tbl.increments()
@@ -19,6 +23,16 @@ exports.up = function(knex) {
             .references('UUID').inTable('users')
             .onDelete('CASCADE').onUpdate('CASCADE')
         tbl.timestamps(true, true)
+    })
+    .createTable('item_sizes', tbl => {
+        tbl.increments()
+        tbl.string('size', 128).notNullable().unique()
+            .onDelete('CASCADE').onUpdate('CASCADE')
+    })
+    .createTable('item_colors', tbl => {
+        tbl.increments()
+        tbl.string('color', 128).notNullable().unique()
+            .onDelete('CASCADE').onUpdate('CASCADE')
     })
 
 }

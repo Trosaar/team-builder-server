627bfb1c9f555e0293e17d8541a40a743ff021d2
Aaron <candyman633@gmail.com>
2020-09-24 20:32:44 -0500

    added restricted middleware for auth routes

---
+++ router-auth/restricted.js
@@ -0,0 +1,22 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
+
+const jwt = require('JsonWebToken');
+const e = require("express");
+
+module.exports = (req, res, next) => {
+    const token = req.header.authorization
+
+    // if we have a token, check it
+    if(token){
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            // if we get an error, they its not valid and display error
+            if(err) {
+                res.status(401).json({ error: "They climbing in your window. Snatching your people up. Hide your kids, hide your wife, hide your password."})
+            } else { // if no error then proceed
+                next()
+            }
+        })
+    } else { //if we dont have a token then display error 
+		res.status(401).json({ message: "what is the flight speed of an unladen swallow?"})
+    }
+}

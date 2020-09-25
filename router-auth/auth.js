d8a8ade35d02f806f245dffbe84b354a63d854d1
Aaron <candyman633@gmail.com>
2020-09-24 20:11:33 -0500

    added get to / route. Need to finish restricted middleware before I can test

--- router-auth/auth.js
+++ router-auth/auth.js
@@ -4,3 +4,20 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const AuthDB = require('./auth-model.js')
 const restrictedMid = require('./restricted.js')
+
+// get post put delete
+// Read Create Update Delete
+
+router.get('/', restrictedMid, async (req, res) => {
+    try {
+        const listOfAllUsers = await AuthDB.findAll();
+
+        listOfAllUsers.forEach(user => {
+            user.password = 'Didnt say the magic word'
+        })
+
+        res.status(200).json({listOfAllUsers})
+    } catch(err) {
+        res.status(500).json({ error: "Looks like something went wrong. Please check the request."})
+    }
+})

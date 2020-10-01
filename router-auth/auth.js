03d87144c163ffa4cd8ee8f95072bead7c9e3626
Aaron <candyman633@gmail.com>
2020-09-30 19:31:40 -0500

    working on auth routes

--- router-auth/auth.js
+++ router-auth/auth.js
@@ -8,6 +8,19 @@ const restrictedMid = require('./restricted.js')
 // get post put delete
 // Read Create Update Delete
 
+function generateToken(user) {
+    const paylaod  = {
+        id: user.id,
+        username: user.username
+    }
+
+    const options = {
+        expiresIn: "8h"
+    }
+
+    return jwt.sign(paylaod, process.env.JWT_SECRET, options)
+}
+
 router.get('/', restrictedMid, async (req, res) => {
     try {
         const listOfAllUsers = await AuthDB.findAll();
@@ -19,5 +32,20 @@ router.get('/', restrictedMid, async (req, res) => {
         res.status(200).json({listOfAllUsers})
     } catch(err) {
         res.status(500).json({ error: "Looks like something went wrong. Please check the request."})
+    }
+})
+
+router.post('/register', async (req, res) => {
+    const userInfo = req.body
+
+    try {
+        userInfo.password = bcrypt.hashSync(userInfo.password, 15)
+
+        const newUser = await AuthDB.add(userInfo)
+        const token = generateToken(newUser)
+
+
+    } catch(err) {
+
     }
 })

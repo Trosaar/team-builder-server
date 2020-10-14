03fedd0ea3606a2b81f61a1f72c6a6361370b4ea
Aaron <candyman633@gmail.com>
2020-10-13 20:17:28 -0500

    get tests for items ready. Working on post

--- router-items/items.test.js
+++ router-items/items.test.js
@@ -2,24 +2,35 @@ const { request } = require("express");
 
 const server = require('../api/server.js')
 const request = require('supertest')
-const db = require('../database/dbConfig.js')
+const db = require('../database/dbConfig.js');
 
 describe('items routes', () => {
     beforeEach( db('items').truncate() )
 
     describe('GET to items', () => {
         it('should return all item from /', () => {
-
+            request(server).get('/api/items').then(res => {
+                expect(res.status).toBe(200)
+            })
         })
 
         it('should return one item from /:id', () => {
-
+            request(server).get('/api/items/:id').then(res => {
+                expect(res.status).toBe(200)
+            })
         })
     })
 
     describe('POST to items', () => {
         it('should return added item from /', () => {
-
+            const testItem = {
+                
+            }
+            
+            request(server).post('/api/items')
+            .send(testItem).then(res => {
+                expect(res.status).toBe(201)
+            })
         })
     })
 

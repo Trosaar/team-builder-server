8fa8f4f76d42afac9e412ebdbb5a0d220cf7fc76
Aaron <candyman633@gmail.com>
2020-10-13 20:03:43 -0500

    first pass of auth testing done

--- router-auth/auth.test.js
+++ router-auth/auth.test.js
@@ -1,14 +1,53 @@
 const server = require('../api/server.js')
 const request = require('supertest')
 const db = require('../database/dbConfig.js')
+const { expectCt } = require('helmet')
 
 describe('Auth Router', () => {
     beforeEach( db('users').truncate() )
 
 
-    describe('get to /', () => {
+    describe('GET to auth', () => {
         it('should return a list of all users if logged in', () => {
-            return request(server).get('/api/auth')
+            request(server).get('/api/').then(res => {
+                expect(res.status).toBe(401)
+            })
         })
+    })
+
+    describe('POST to auth', ()=> {
+        it('should return the token for a new user', () => {
+            const user = {
+                username: "test",
+                password: "testpass"
+            }
+
+            request(server).post('/api/register')
+            .send(user).then(res => {
+                expect(res.status).toBe(201)
+                expect(res.body.username).toBe("test")
+            })
+        })
+
+        it('should return a token for existing user', () => {
+            const user = {
+                username: "test",
+                password: "testpass"
+            }
+            
+            request(server).post('/api/login')
+            .send(user).then(res => {
+                expect(res.status).toBe(200)
+                expect(res.body).toContain(token)
+            })
+        })
+    })
+
+    xdescribe('PUT to auth', () => {
+
+    })
+
+    xdescribe('DELETE to auth', () => {
+
     })
 })
